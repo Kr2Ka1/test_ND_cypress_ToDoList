afd9c06be7256bf26c8cf5b95bc1d02dd03b0a2c
Kr2Ka1 <kriste.kore.flora@gmail.com>
2025-02-14 11:19:48 +0200

    nd analizė

--- cypress/e2e/Cypress_ToDoList_Tests/Cypress_ToDoList.cy.js
+++ cypress/e2e/Cypress_ToDoList_Tests/Cypress_ToDoList.cy.js
@@ -1,19 +1,25 @@
+///<reference types="cypress" />
+
 it('To Do List Test', () => {
     cy.visit('https://todolist.james.am/#/')
     cy.get('header').should('be.visible');
     cy.contains('h1', 'To Do List').should('be.visible');
+    // cy.get('header h1').should('have.text', 'To Do List');
+    //cy.get('header h1').should('exist');
+    //cy.get('header h1').should('be.visible');
     cy.get('footer').should('exist');
     cy.get('footer p').should('exist');
-    cy.get('footer').contains('p', 'Double-click to edit a todo').should('be.visible');
     cy.get('footer p').invoke('text').then((text) => {
         cy.log('Footer tekstas:', text);
     });
+    cy.get('footer').contains('p', 'Double-click to edit a todo').should('be.visible');
 });
 
 
 it('is placeholder visible', () => {
     cy.visit('https://todolist.james.am/#/');
-    cy.get('input').should('have.attr', 'placeholder', "What need's to be done?");
+    cy.get('input.new-todo').should('have.attr', 'placeholder', "What need's to be done?");
+    cy.get('input[placeholder="What need\'s to be done?"]').should('exist').should('be.visible');
 });
 
 it('ne tuščias sąrašas', () => {
@@ -22,4 +28,13 @@ it('ne tuščias sąrašas', () => {
     cy.get('input.new-todo').type('2 uzduotis{enter}');
     cy.get('input.new-todo').type('3 uzduotis{enter}');
     cy.get('ul.todo-list li').should('have.length.at.least', 2);
+    cy.get('ul.todo-list li').each(($el) => {
+        cy.log($el)
+    });
+    cy.get('ul.todo-list li')
+    .each(($el, $list) => {
+        cy.log($el, $list)})
+        .then(($list)=>{
+expect($list).to.have.length(3);
+        })
 });
